0ef6a6b1a928fe788ddc569d454b5fac856ef09c
opuerto <oscar.puertohn@gmail.com>
2021-08-25 22:38:39 +0200

    Adding Routing Adding Sass

---
+++ src/pages/Feed/Feed.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from "react-bootstrap/Button";
+import "./Feed.scss";
+
+
+
+
+export default function Feed() {
+  return (
+    <>
+      <h1>FEED PAGE</h1>
+      <Link to="/restaurants">Go To Restaurants</Link>
+      <Container>
+        <Row>
+          <Col xs={12} md={6} className="fee">1 of 2</Col>
+          <Col xs={12} md={6} className="col-c">2 of 2</Col>
+        </Row>
+        <Row>
+          <Col>1 of 3</Col>
+          <Col>2 of 3</Col>
+          <Col>3 of 3</Col>
+        </Row>
+      </Container>
+
+    </>
+  )
+} 
