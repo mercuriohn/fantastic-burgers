7eb15fd772d0bda5edb74477fdd6ae344af912ca
opuerto <oscar.puertohn@gmail.com>
2021-08-25 23:13:20 +0200

    add css

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import "./Feed.css";
 
 
 
@@ -15,8 +15,8 @@ export default function Feed() {
       <Link to="/restaurants">Go To Restaurants</Link>
       <Container>
         <Row>
-          <Col xs={12} md={6} className="fee">1 of 2</Col>
-          <Col xs={12} md={6} className="col-c">2 of 2</Col>
+          <Col xs={12} md={6} className="green-background">1 of 2</Col>
+          <Col xs={12} md={6} className="red-background">2 of 2</Col>
         </Row>
         <Row>
           <Col>1 of 3</Col>
